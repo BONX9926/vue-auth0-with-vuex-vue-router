ecc33338d74612dfa6f2beccde133747e1c1cec7
winhackger <winhackger@gmail.com>
2019-05-14 23:33:52 +0700

    Add initial Page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,13 @@
 <template>
-  <HelloWorld />
+  <h1>Home page</h1>
 </template>
 
 <script>
-  import HelloWorld from '../components/HelloWorld'
+  // import HelloWorld from '../components/HelloWorld'
 
-  export default {
-    components: {
-      HelloWorld
-    }
-  }
+  // export default {
+  //   components: {
+  //     HelloWorld
+  //   }
+  // }
 </script>
